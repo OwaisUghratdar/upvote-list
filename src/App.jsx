2390cb3fda3259486236ce03c17e676265c2259e
Owais Ughratdar <owais.ughratdar@gmail.com>
2025-02-25 23:54:49 -0500

    fix: update initial state

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import UpvoteList from './components/UpvoteList'
 function App() {
   return (
     <div style={{ maxWidth: '800px', margin: '2rem auto', padding: '1rem' }}>
-      <UpvoteList initialCount={3} />
-      <UpvoteList initialCount={2} />
-      <UpvoteList initialCount={5} />
+      <UpvoteList initialCount={0} />
+      <UpvoteList initialCount={0} />
+      <UpvoteList initialCount={0} />
     </div>
   )
 }
